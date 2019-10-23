b15bf22d19305092b26a5c5c21079cb411290ff2
andrei <ionitaandrei3@gmail.com>
2019-10-23 14:17:50 +0300

    adding dynamic port to listen to

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.put("/image", (req, res) => {image.handleImage(req, res, db)});
 
 app.post("/imageurl", (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log("I am listening...");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("I am listening on ${process.env.PORT}");
 });
 
 /*
