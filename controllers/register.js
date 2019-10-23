291499d2277726ea43ba66884cf4fe9b293e59d6
andrei <ionitaandrei3@gmail.com>
2019-10-23 15:52:05 +0300

    registration handler corrected

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,6 @@
-const handleRegister = (req, res, db, bcrypt, knex) => {
+const handleRegister = (req, res, db, bcrypt) => {
     const{email, name, password} = req.body;
-    if(!email || !name || password) {
+    if(!email || !name || !password) {
         return res.status(400).json("incorrect form submission");
     } else {
         const hash = bcrypt.hashSync(password);
